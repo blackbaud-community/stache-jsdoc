459c0d7248bb86791e9a35e5aa907c10bcc85aea
Bobby Earl <bobby.earl@blackbaud.com>
2015-07-28 13:57:30 -0400

    Correctly merging config options

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,10 @@
 "use strict";
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        'stache_jsdoc': {
-            options: {
-                tmpPath: '.tmp-convert/',
-                demoHtmlPath: 'docs/demo.html',
-                demoJsPath: 'docs/demo.js',
-                src: 'test/*/*.js',
-                dest: 'test/data.json'
-            },
-            jsdoc2md: {
-                separateOutputFilePerInput: {
-                    options: {
-                        json: true
-                    },
-                    files: [{
-                        expand: true,
-                        ext: '.json',
-                        src: '<%= stache_jsdoc.options.src %>',
-                        dest: '<%= stache_jsdoc.options.tmpPath %>'
-                    }]
-                }
-            }
-        },
-        jsdoc2md: {
-            oneOutputFile: '<%= stache_jsdoc.jsdoc2md.oneOutputFile %>',
-            separateOutputFilePerInput: '<%= stache_jsdoc.jsdoc2md.separateOutputFilePerInput %>'
+    grunt.config.init({
+        stache_jsdoc: {
+            src: 'test/*/*.js',
+            dest: 'test/data.json'
         }
     });
 
--- tasks/stache-jsdoc.js
+++ tasks/stache-jsdoc.js
@@ -5,6 +5,29 @@
  */
 module.exports = function (grunt) {
 
+    var settings = {},
+        defaults = {
+            tmpPath: '.tmp-convert/',
+            demoPrefix: 'example-',
+            demoFiles: [
+                'docs/demo.html',
+                'docs/demo.js'
+            ],
+            jsdoc2md: {
+                separateOutputFilePerInput: {
+                    options: {
+                        json: true
+                    },
+                    files: [{
+                        expand: true,
+                        ext: '.json',
+                        src: '<%= stache_jsdoc.src %>',
+                        dest: '<%= stache_jsdoc.tmpPath %>'
+                    }]
+                }
+            }
+        };
+
     /**
      * Grabs the applicable config options from stache-jsdoc and passes them to jsdoc2md task.
      * Queue's up the jsdoc2md and stache-jsdoc-post callback tasks.
@@ -12,46 +35,48 @@ module.exports = function (grunt) {
     grunt.task.registerTask(
         'stache_jsdoc',
         'Convert JSDoc to Blackbaud Stache',
-        [
-            'jsdoc2md:separateOutputFilePerInput',
-            'stache-jsdoc-post'
-        ]
+        function () {
+            grunt.config.set('stache_jsdoc', this.options(defaults));
+            grunt.config.set('jsdoc2md', grunt.config.get('stache_jsdoc.jsdoc2md'));
+            grunt.task.run([
+                'jsdoc2md:separateOutputFilePerInput',
+                'stache-jsdoc-post'
+            ]);
+        }
     );
 
     /**
      * The real reason for this package to even exist.
      * Adds additional Blackbaud specific details to the created JSON file.
      */
-    grunt.task.registerTask(
-        'stache-jsdoc-post',
-        'Automatically called after running jsdoc2md task',
-        function () {
+    grunt.task.registerTask('stache-jsdoc-post', function () {
+        var src = grunt.config.get('stache_jsdoc.tmpPath'),
+            dest = grunt.config.get('stache_jsdoc.dest'),
+            files = grunt.file.expand(src + '/**/*.json'),
+            combined = [];
 
-            var src = grunt.config.get('stache_jsdoc.options.tmpPath'),
-                dest = grunt.config.get('stache_jsdoc.options.dest'),
-                files = grunt.file.expand(src + '/**/*.json'),
-                combined = [],
-                addFile = function (json, folder, key) {
-                    var file = folder + 'docs/demo.' + key;
-                    if (grunt.file.exists(file)) {
-                        json['example-' + key] = grunt.file.read(file);
+        grunt.file.expand(src + '/**/*.json').forEach(function (el) {
+            var json = grunt.file.readJSON(el),
+                file = el.replace(src, ''),
+                folder = file.substr(0, file.lastIndexOf('/') + 1),
+                demoPrefix = grunt.config.get('stache_jsdoc.demoPrefix'),
+                demoFiles = grunt.config.get('stache_jsdoc.demoFiles');
+
+            // Future implements may need to adjust this
+            if (json && json.length === 1) {
+                json = json[0];
+                demoFiles.forEach(function (file) {
+                    var path = folder + file,
+                        ext = file.substr(file.lastIndexOf('.') + 1);
+                    if (grunt.file.exists(path)) {
+                        json[demoPrefix + ext] = grunt.file.read(path);
                     }
-                }
-
-            grunt.file.expand(src + '/**/*.json').forEach(function (el) {
-                var json = grunt.file.readJSON(el)[0],
-                    file = el.replace(src, ''),
-                    folder = file.substr(0, file.lastIndexOf('/') + 1);
-                addFile(json, folder, 'html');
-                addFile(json, folder, 'js');
+                });
                 combined.push(json);
-            });
+            }
+        });
 
-            grunt.log.writeln('Writing ' + dest);
-            grunt.file.write(
-                dest,
-                JSON.stringify(combined, null, 2)
-            );
-        }
-    );
+        grunt.log.writeln('Writing ' + dest);
+        grunt.file.write(dest, JSON.stringify(combined, null, 2));
+    });
 };
